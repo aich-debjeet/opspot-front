a7ee691a07b3227e7a897b17f075be0d6479c622
Gayatri Kawade <gayatri.kawade@aeione.com>
2019-06-26 16:21:07 +0530

    wip:step1form

--- src/app/modules/auth/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/forgot-password/forgot-password.component.ts
@@ -9,7 +9,7 @@ import { Session } from '../../../services/session';
 import Swal from 'sweetalert2';
 import { LoginComponent } from '../login.component';
 import { LoginForm } from '../../forms/login/login';
-import { Form, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Form, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -29,6 +29,7 @@ export class ForgotPasswordComponent {
 
   // step1
   step1Form: FormGroup;
+  mobileNumber
 
   // ste2
   step2Form: FormGroup;
@@ -50,34 +51,45 @@ export class ForgotPasswordComponent {
     public formBuilder: FormBuilder
   ) {
     this.step1Form = this.formBuilder.group({
-      mobileEmail: ['', [Validators.required]]
+      mobileNumber: ['', [Validators.required]]
     }),
       this.step2Form = this.formBuilder.group({
-        otp: this.formBuilder.group({
-          otp1: ['', [Validators.required]], 
-          otp2: ['', [Validators.required]], 
-          otp3: ['', [Validators.required]], 
-          otp4: ['', [Validators.required]], 
-          otp5: ['', [Validators.required]], 
-          otp6: ['', [Validators.required]]
-        })
+        // otp: this.formBuilder.group({
+        otpNum1: ['', [Validators.required]],
+        otpNum2: ['', [Validators.required]],
+        otpNum3: ['', [Validators.required]],
+        otpNum4: ['', [Validators.required]],
+        otpNum5: ['', [Validators.required]],
+        otpNum6: ['', [Validators.required]]
       })
+    // })
+    this.step3Form = this.formBuilder.group({
+      newPassword: ['', [Validators.required]],
+      confirmPassword: ['', []]
+    }, { validators: this.passwordConfirmcheck })
   }
 
 
 
+  passwordConfirmcheck(c: AbstractControl): { invalid: boolean } {
+    if (c.get('password').value !== c.get('confirm_password').value) {
+      c.get('confirm_password').setErrors({ 'noMatch': true });
+      return { invalid: true };
+    }
+  }
+
   ngOnInit() {
-    this.title.setTitle('Forgot Password');
+    // this.title.setTitle('Forgot Password');
 
-    this.paramsSubscription = this.route.params.subscribe((params) => {
-      if (params['code']) {
-        this.setCode(params['code']);
-      }
+    // this.paramsSubscription = this.route.params.subscribe((params) => {
+    //   if (params['code']) {
+    //     this.setCode(params['code']);
+    //   }
 
-      if (params['username']) {
-        this.username = params['username'];
-      }
-    });
+    //   if (params['username']) {
+    //     this.username = params['username'];
+    //   }
+    // });
   }
 
   ngOnDestroy() {
@@ -90,70 +102,92 @@ export class ForgotPasswordComponent {
     // this.inProgress = true;
     this.submitted1 = true;
     if (this.step1Form.valid)
-      this.step = 2;
-    // this.client.post('api/v1/forgotpassword/request', {
-    //   username: username.value
-    // })
-    //   .then((data: any) => {
-    //     username.value = '';
-
-    //     this.inProgress = false;
-    //     this.step = 2;
-    //   })
-    //   .catch((e) => {
-
-    //     this.inProgress = false;
-    //     if (e.status === 'failed') {
-    //       this.error = 'There was a problem trying to reset your password. Please try again.';
-    //     }
-
-    //     if (e.status === 'error') {
-    //       this.error = e.message;
-    //     }
-
-    //   });
-  }
-
-  setCode(code: string) {
-    this.step = 3;
-    this.code = code;
-  }
-
-  validatePassword(password) {
-    if (/@/.test(password.value)) {
-      this.error = '@ is not allowed';
-    } else {
-      this.error = null;
-    }
-  }
-
-  reset(password) {
-    if (!this.error) {
-      this.client.post('api/v1/forgotpassword/reset', {
-        password: password.value,
-        code: this.code,
-        username: this.username
+      // this.step = 2;
+      this.client.post('api/v3/verification/mobile/verify', {
+        number: this.step1Form.value.mobileNumber
       })
-        .then((response: any) => {
-          this.session.login(response.user);
-          this.router.navigate(['/newsfeed']);
+        .then((data: any) => {
+          // username.value = '';
+
+          this.inProgress = false;
+          this.step = 2;
         })
         .catch((e) => {
-          this.error = e.message;
-          setTimeout(() => {
-            this.router.navigate(['/login']);
-          }, 2000);
+          this.inProgress = false;
+          if (e.status === 'failed') {
+            this.error = 'There was a problem trying to reset your password. Please try again.';
+          }
+          if (e.status === 'error') {
+            this.error = e.message;
+          }
         });
-    }
   }
+
+  // setCode(code: string) {
+  //   this.step = 3;
+  //   this.code = code;
+  // }
+
+  // validatePassword(password) {
+  //   if (/@/.test(password.value)) {
+  //     this.error = '@ is not allowed';
+  //   } else {
+  //     this.error = null;
+  //   }
+  // }
+
+  // reset(password) {
+  //   if (!this.error) {
+  //     this.client.post('api/v1/forgotpassword/reset', {
+  //       password: password.value,
+  //       code: this.code,
+  //       username: this.username
+  //     })
+  //       .then((response: any) => {
+  //         this.session.login(response.user);
+  //         this.router.navigate(['/newsfeed']);
+  //       })
+  //       .catch((e) => {
+  //         this.error = e.message;
+  //         setTimeout(() => {
+  //           this.router.navigate(['/login']);
+  //         }, 2000);
+  //       });
+  //   }
+  // }
 
 
   submitted2 = false;
-  showForm3() {
+  validateOtp() {
     this.submitted2 = true;
+    if (this.step2Form.valid)
+      this.step = 3;
+  }
+
+  submitted3 = false;
+  updatePassword() {
+    this.submitted3 = true;
     this.step = 3;
   }
 
+  nextOtpNum(event, pos: number) {
+    let nextInput = event.srcElement.nextElementSibling; // get the sibling element
+    let previous = event.srcElement.previousElementSibling; //get the previous
+    console.log(event)
+    var target = event.target || event.srcElement;
+    var id = target.id
+
+    if (event.keyCode === 8) {
+      if (event.srcElement.previousElementSibling === null) {
+        return;
+      }
+      else { previous.focus(); }
+    }
+    else if (nextInput == null)  // check the maxLength from here
+      return;
+    else
+      nextInput.focus();
+  }
 
 
 }
