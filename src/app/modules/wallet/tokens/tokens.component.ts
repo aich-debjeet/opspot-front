88e8d47459f2628d6004071ed5798d4eeb42fda3
shashivnaad <shashikumarv@vnaad.com>
2019-08-06 13:04:05 +0530

    wallet time view added

--- src/app/modules/wallet/tokens/tokens.component.ts
+++ src/app/modules/wallet/tokens/tokens.component.ts
@@ -1,19 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Client } from '../../../services/api/client';
+
 
 @Component({
   moduleId: module.id,
   selector: 'm-wallet--tokens',
   templateUrl: 'tokens.component.html'
-})
-export class WalletTokensComponent {
+}) 
+export class WalletTokensComponent implements OnInit {
 
   showOnboarding: boolean = false;
   opspot = window.Opspot;
 
-  constructor(route: ActivatedRoute) {
+
+  
+  constructor(route: ActivatedRoute, protected client: Client,protected cd: ChangeDetectorRef,) {
     route.url.subscribe(() => {
       this.showOnboarding = route.snapshot.firstChild && route.snapshot.firstChild.routeConfig.path === 'transactions';
     });
   }
+  
+  overview = {
+    nextPayout: null,
+  }
+ protected updateTimer$;
+  ngOnInit(){
+    this.load();
+    this.updateTimer$ = setInterval(this.updateNextPayout.bind(this), 1000);
+
+  }
+
+  async load() {
+    try {
+      const result: any = await this.client.get(`api/v2/blockchain/contributions/overview`);
+      this.overview.nextPayout = result.nextPayout;
+      this.detectChanges();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  
+   detectChanges(){
+    this.cd.markForCheck(); 
+    this.cd.detectChanges();
+   }
+ 
+   updateNextPayout(){
+    if (this.overview.nextPayout) {
+      this.overview.nextPayout--;
+      this.detectChanges();
+    }
+   }
+
 }
