144bec5c1875822c400ba04c8679c787822c92d0
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-09-28 15:04:33 +0530

    linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,11 @@ import { TextMaskModule } from 'angular2-text-mask';
 
 import { Opspot } from './app.component';
 
-import { OPSPOT_APP_ROUTING_DECLARATIONS, OpspotAppRoutes, OpspotAppRoutingProviders } from './router/app';
+import {
+  OPSPOT_APP_ROUTING_DECLARATIONS,
+  OpspotAppRoutes,
+  OpspotAppRoutingProviders
+} from './router/app';
 
 import { OPSPOT_DECLARATIONS } from './declarations';
 import { OPSPOT_PLUGIN_DECLARATIONS } from './plugin-declarations';
@@ -39,7 +43,7 @@ import { OnboardingModule } from './modules/onboarding/onboarding.module';
 import { NotificationModule } from './modules/notifications/notification.module';
 
 import { GroupsModule } from './modules/groups/groups.module';
-import { NetworkModule } from './modules/network/network.module'
+import { NetworkModule } from './modules/network/network.module';
 import { PostMenuModule } from './common/components/post-menu/post-menu.module';
 import { BanModule } from './modules/ban/ban.module';
 import { BlogModule } from './modules/blogs/blog.module';
@@ -58,8 +62,8 @@ import { JobsMarketingModule } from './modules/jobs/jobs.module';
 import { HelpdeskModule } from './modules/helpdesk/helpdesk.module';
 import { MobileModule } from './modules/mobile/mobile.module';
 import { CanaryModule } from './modules/canary/canary.module';
-import { HttpClientModule } from "@angular/common/http";
-import { NgxIntlTelInputModule }  from 'ngx-intl-tel-input';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { BookmarkModule } from './modules/bookmark/bookmark.module';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
@@ -71,11 +75,8 @@ import { ExploreBlogsComponent } from './modules/explore/explore-blogs/explore-b
 import { InviteFriendsModule } from './modules/invite-friends/invite-friends.module';
 import { PortfolioModule } from './modules/portfolio/portfolio.module';
 
-
 @NgModule({
-  bootstrap: [
-    Opspot
-  ],
+  bootstrap: [Opspot],
   declarations: [
     Opspot,
     OPSPOT_APP_ROUTING_DECLARATIONS,
@@ -124,7 +125,7 @@ import { PortfolioModule } from './modules/portfolio/portfolio.module';
     NewsfeedModule,
     MediaModule,
     AuthModule,
-    BlockchainModule,    
+    BlockchainModule,
     BlockchainMarketingModule,
     NodesMarketingModule,
     BrandingModule,
@@ -147,11 +148,8 @@ import { PortfolioModule } from './modules/portfolio/portfolio.module';
   providers: [
     OpspotAppRoutingProviders,
     OPSPOT_PROVIDERS,
-    OPSPOT_PLUGIN_PROVIDERS,
+    OPSPOT_PLUGIN_PROVIDERS
   ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class OpspotModule {
-}
+export class OpspotModule {}
