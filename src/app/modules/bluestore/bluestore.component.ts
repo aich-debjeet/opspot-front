b42d2a02f9b6d7bef1b9c48616d2831aea68ca9d
Gayatri Kawade <gayatri.kawade@aeione.com>
2019-09-30 10:28:54 +0530

    Added: bluestore module

---
+++ src/app/modules/bluestore/bluestore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bluestore',
+  templateUrl: './bluestore.component.html',
+  styleUrls: ['./bluestore.component.scss']
+})
+export class BluestoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/bluestore/bluestore.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BluestoreComponent } from './bluestore.component';
+
+@NgModule({
+  declarations: [BluestoreComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BluestoreModule { }
