de227cbd6cb983a18326452d14fbfae3dcf0ea9c
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2019-10-02 14:07:12 +0530

    update: displaying basic entity values

--- src/app/modules/legacy/components/cards/post-card/post-card.ts
+++ src/app/modules/legacy/components/cards/post-card/post-card.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'opspot-post-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-card.scss']
 })
 export class PostCard implements OnInit {
+
+  @Input() entity: any;
 
   constructor() { }
 
