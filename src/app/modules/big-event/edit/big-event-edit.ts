e413ab0e52473998c9501aa78cf3b8b725955273
gayatri <gayatrikawade80@gmail.com>
2019-10-21 14:16:23 +0530

    Add: big event edit

---
+++ src/app/modules/big-event/edit/big-event-edit.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-big-event-edit',
+  templateUrl: './big-event-edit.html',
+  styleUrls: ['./big-event-edit.scss']
+})
+export class BigEventEdit implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+
+
+
+
+}
