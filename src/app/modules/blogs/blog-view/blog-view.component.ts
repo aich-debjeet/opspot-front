95db03891ced0cc4b6145bfcbbbbaf58854ddfba
Debjeet Aich <debjeet.aich@aeione.com>
2019-11-08 17:11:22 +0530

    unsaved changes

--- src/app/modules/blogs/blog-view/blog-view.component.ts
+++ src/app/modules/blogs/blog-view/blog-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Client } from '../../../services/api';
+import { Session } from '../../../services/session';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-view',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-view.component.scss']
 })
 export class BlogViewComponent implements OnInit {
+  
+  @Input() entity: any;
 
-  constructor() { }
+  canDelete: boolean = false;
+  isTranslatable: boolean;
+  menuOptions: Array<string> = ['edit', 'delete'];
+
+  constructor(
+    public router: Router,
+    public client: Client,
+    public session: Session
+  ) { }
 
   ngOnInit() {
   }
+  menuOptionSelected(option: string) {
+    switch (option) {
+      case 'edit':
+        this.router.navigate(['/blog/edit', this.entity.guid]);
+        break;
+      case 'delete':
+        this.delete();
+        break;
+    }
+  }
 
+  delete() {
+    this.client.delete('api/v1/blog/' + this.entity.guid)
+      .then((response: any) => {
+        this.router.navigate(['/blog/owner']);
+      });
+  }
 }
