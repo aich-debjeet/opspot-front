7d18b16d034cb52aba4d55027d60d7a7c5717bb1
gayatrikawade <you@example.com>
2019-11-15 18:31:11 +0530

    removed console log

--- src/app/common/components/opportunity-list-card/opportunity-list-card.component.ts
+++ src/app/common/components/opportunity-list-card/opportunity-list-card.component.ts
@@ -31,7 +31,6 @@ export class OpportunityListCardComponent implements OnInit {
 
   set _object(value){
     this.opportunity = value;
-    console.log('this.opportunity: ', this.opportunity);
   }
 
 
