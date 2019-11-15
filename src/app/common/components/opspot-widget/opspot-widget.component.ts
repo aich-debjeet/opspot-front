dec2b949339e3846d9c89b88a0820bd2bac1f2c5
gayatrikawade <you@example.com>
2019-11-15 19:18:23 +0530

    updated specialhashtag function

--- src/app/common/components/opspot-widget/opspot-widget.component.ts
+++ src/app/common/components/opspot-widget/opspot-widget.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Client } from '../../../services/api/client';
 import { Session } from '../../../services/session';
+import { OpspotUser } from '../../../../app/interfaces/entities';
+import { SpecialHashtg } from '../../../helpers/special-hashtag';
 
 @Component({
   selector: 'opspot-widget',
@@ -9,35 +11,61 @@ import { Session } from '../../../services/session';
 export class OpspotWidgetComponent implements OnInit {
 
   @Input() entityType: any;
+  @Input() user: any;
 
   entities: any[];
   reqType: string;
+  inProgress: boolean = false;
+  opspot = window.Opspot;
+  // user: OpspotUser;
+
+
   constructor(
     private client: Client,
-    private session: Session
+    private session: Session,
   ) { }
 
   ngOnInit() {
     this.geteData();
   }
 
+  params: any = {
+    // TODO @abhijeet check for all valid request params
+    taxonomies: 'activity',
+    offset: '',
+    limit: 12,
+    rating: 2,
+    q: ''
+  };
+
+
   geteData() {
-    console.log('Widget geteData()');
+    // console.log('Widget geteData()');
     // this.inProgress = true;
     let ownerGuid = this.session.getLoggedInUser().guid;
+    let endPoint;
     if (this.entityType === 'opportunity') {
       this.reqType = 'opportunities';
-    } else {
+      endPoint = `api/v2/feeds/container/${ownerGuid}/${this.reqType}?limit=3&sync=&as_activities=&force_public=1`
+    } else if (this.entityType === 'event') {
       this.reqType = 'events';
+      endPoint = `api/v2/feeds/container/${ownerGuid}/${this.reqType}?limit=3&sync=&as_activities=&force_public=1`
+    } else {
+      if (this.user !== "") {
+        this.reqType = this.user.username + "'s " + 'Journey'
+        this.params.q = SpecialHashtg.concat('myjourney',this.user.username)
+      }
+      endPoint = `api/v2/search`
     }
-    this.client.get(`api/v2/feeds/container/${ownerGuid}/${this.reqType}?limit=3&sync=&as_activities=&force_public=1`)
+    this.client.get(endPoint, this.params)
       .then((data: any) => {
         if (data && data.entities) {
           this.entities = data.entities;
         }
+        this.inProgress = false;
       })
       .catch((e) => {
-        // this.inProgress = false;
+        this.inProgress = false;
       });
   }
 
