8402adcd29a0b50fa22a7986c655da3d79c5cdcd
Gayatri Kawade <gayatrikawade80@gmail.com>
2019-12-31 14:54:10 +0530

    bluestore carasoule fixes

--- src/app/modules/bluestore/bluestore.component.ts
+++ src/app/modules/bluestore/bluestore.component.ts
@@ -240,7 +240,35 @@ export class BluestoreComponent implements OnInit {
       });
   }
 
-  slideConfig = { slidesToShow: 6, slidesToScroll: 1, arrows: true };
+  slideConfig = { 
+    slidesToShow: 6, 
+    slidesToScroll: 1, 
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }]
+     };
 
   slickInit(e) {
     // console.log('slick initialized in activity');
@@ -337,5 +365,9 @@ export class BluestoreComponent implements OnInit {
   ngOnDestroy() {
     this.paramsSubscription.unsubscribe();
   }
+
+  next() {}
+
+  prev() {}
 
 }
