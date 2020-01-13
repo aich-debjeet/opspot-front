a3e63408de20ec05a99318eacbc9316ffe234de3
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2020-01-13 17:37:41 +0530

    fix: home user card

--- src/app/modules/legacy/components/cards/user/user.ts
+++ src/app/modules/legacy/components/cards/user/user.ts
@@ -20,35 +20,37 @@ export class UserCard implements OnInit {
   offset: string = '';
   subscriptionCount;
   subscriberCount;
-   
-  constructor(public session: Session, public client: Client) {
-  }
-  
+
+  constructor(
+    public session: Session,
+    public client: Client
+  ) { }
+
   set object(value: any) {
     this.user = value;
     this.bannerSrc = `${this.opspot.cdn_url}fs/v1/banners/${this.user.guid}/fat/${this.user.icontime}`;
-    if(this.user){
-      this.subscriptionCount= this.user.subscriptions_count;
+    if (this.user) {
+      this.subscriptionCount = this.user.subscriptions_count;
       this.subscriberCount = this.user.subscribers_count;
     }
   }
-ngOnInit(){
-  
-  /* 
-    @sashi/anup: quick fix TODO:
-    Count could be found from user object. There is no necessity to call an api for that which fetches complete data on pagination.
-  */
-  // this.client.get('api/v1/subscribe/subscriptions/' + this.user.guid, { offset: this.offset })
-  // .then(res=>{
-  //  this.subscriptionCount=Math.abs(res['users'].length-1)
-  // })
-  // .catch(err=>console.log(err))
-  
-  // this.client.get('api/v1/subscribe/subscribers/' + this.user.guid, { offset: this.offset })
-  // .then(res=>{
-  //  this.subscriberCount=Math.abs(res['users'].length)
-  // })
-  // .catch(err=>console.log(err))
 
-}
+  ngOnInit() {
+    /* 
+      @sashi/anup: quick fix TODO:
+      Count could be found from user object. There is no necessity to call an api for that which fetches complete data on pagination.
+    */
+    // this.client.get('api/v1/subscribe/subscriptions/' + this.user.guid, { offset: this.offset })
+    // .then(res=>{
+    //  this.subscriptionCount=Math.abs(res['users'].length-1)
+    // })
+    // .catch(err=>console.log(err))
+
+    // this.client.get('api/v1/subscribe/subscribers/' + this.user.guid, { offset: this.offset })
+    // .then(res=>{
+    //  this.subscriberCount=Math.abs(res['users'].length)
+    // })
+    // .catch(err=>console.log(err))
+
+  }
 }
