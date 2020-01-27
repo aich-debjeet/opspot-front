0d1314228426e44b57570d17eac3681556939a54
Debjeet Aich <debjeet.aich@aeione.com>
2020-01-27 14:28:56 +0530

    Rewards WIP

--- src/app/modules/wallet/tokens/contributions/chart.component.ts
+++ src/app/modules/wallet/tokens/contributions/chart.component.ts
@@ -1,21 +1,48 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
 
 import { Client } from '../../../../services/api/client';
-import { Session } from '../../../../services/session';
+// import { Session } from '../../../../services/session';
 
 @Component({
   selector: 'm-wallet-token--chart',
   templateUrl: 'chart.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WalletTokenContributionsChartComponent {
-
+export class WalletTokenContributionsChartComponent implements OnInit {
+  rewards: any[];
+  sampleObj:{}= {"comments": 4, "reminds": 4,"votes": 2,"subscribers": 4,"referrals": 50,"checkin": 2,"signup": 1.575,"profile": 0.7875};
+  
   constructor(
-    protected client: Client,
-    protected cd: ChangeDetectorRef,
-    public session: Session,
-    protected router: Router,
+    protected client: Client
   ) { }
+  
+  ngOnInit() {
+    this.load();
+  }
+  
+  load() {
+    this.client.get('api/v3/rewards/data').then((result: any) => {
+      // this.prepare(result.entities);
+      if (result.entities) {
+        this.rewards = result.entities;
+        console.log(this.rewards);
+        // this.convertToObj(this.rewards);
+      }
+    });
+  }
+  convertToObj(array:any){
+    console.log(array);
+    for (var i = 0; i < array.length; i++) {
+      this.sampleObj[array[i]] = array[i].value;
+    }
+    console.log('the refined object is',this.sampleObj);
+  }
 
+  // prepare(entities) {
+  //   for (var i = 0; i < entities.length; i++) {
+  //     this.rewards[entities[i].key] = entities[i].value;
+  //   }
+  //   console.log('after', this.rewards);
+  // }
 }
