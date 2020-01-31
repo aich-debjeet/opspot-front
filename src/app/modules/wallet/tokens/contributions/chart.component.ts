6fe4cb1f91d00b549a0a59762136fc58613a4963
Debjeet Aich <debjeet.aich@aeione.com>
2020-01-31 12:19:55 +0530

    Add: Dynamic reward Points

--- src/app/modules/wallet/tokens/contributions/chart.component.ts
+++ src/app/modules/wallet/tokens/contributions/chart.component.ts
@@ -6,43 +6,22 @@ import { Client } from '../../../../services/api/client';
 
 @Component({
   selector: 'm-wallet-token--chart',
+  inputs:['_setRewards:rewards'],
   templateUrl: 'chart.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WalletTokenContributionsChartComponent implements OnInit {
-  rewards: any[];
-  sampleObj:{}= {"comments": 4, "reminds": 4,"votes": 2,"subscribers": 4,"referrals": 50,"checkin": 2,"signup": 1.575,"profile": 0.7875};
-  
+  rewards: any;
+  display: boolean =false;
   constructor(
     protected client: Client
-  ) { }
+  ) {
+   }
   
   ngOnInit() {
-    this.load();
-  }
-  
-  load() {
-    this.client.get('api/v3/rewards/data').then((result: any) => {
-      // this.prepare(result.entities);
-      if (result.entities) {
-        this.rewards = result.entities;
-        console.log(this.rewards);
-        // this.convertToObj(this.rewards);
-      }
-    });
-  }
-  convertToObj(array:any){
-    console.log(array);
-    for (var i = 0; i < array.length; i++) {
-      this.sampleObj[array[i]] = array[i].value;
-    }
-    console.log('the refined object is',this.sampleObj);
   }
 
-  // prepare(entities) {
-  //   for (var i = 0; i < entities.length; i++) {
-  //     this.rewards[entities[i].key] = entities[i].value;
-  //   }
-  //   console.log('after', this.rewards);
-  // }
+  set _setRewards(value: any) {
+    this.rewards = value;
+  }
 }
