6e2925587de4ca004484dd2590000e603610da59
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2020-02-19 20:23:57 +0530

    add: terms and other static pages

---
+++ src/app/modules/static/enrollment-terms/enrollment-terms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enrollment-terms',
+  templateUrl: './enrollment-terms.component.html',
+  styleUrls: ['./../static.component.scss']
+})
+export class EnrollmentTermsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
