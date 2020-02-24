d4480a9fdc372e347473ad77cfe74c27e60c754d
Abhijeet Salunkhe <abhijeet.salunkhe@aeione.com>
2020-02-24 19:06:45 +0530

    add: organization search

--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -12,6 +12,7 @@ import { SearchBarComponent } from './bar.component';
 import { SearchComponent } from './search.component';
 import { SearchFilterComponent } from './filter/search-filter.component';
 import { LegacyModule } from '../legacy/legacy.module';
+import { OrganizationModule } from '../organization/organization.module';
 
 const searchRoutes: Routes = [
   { path: 'search', component: SearchComponent }
@@ -24,7 +25,8 @@ const searchRoutes: Routes = [
     ReactiveFormsModule,
     RouterModule.forChild(searchRoutes),
     CommonModule,
-    LegacyModule
+    LegacyModule,
+    OrganizationModule
   ],
   declarations: [
     SearchHybridListComponent,
