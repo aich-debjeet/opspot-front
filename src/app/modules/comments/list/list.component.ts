3b9f9a367ed3aee3d3ab943203ed7d48944bf6df
Debjeet Aich <debjeet.aich@aeione.com>
2020-03-16 16:42:57 +0530

    fix:temp fix for comment guid

--- src/app/modules/comments/list/list.component.ts
+++ src/app/modules/comments/list/list.component.ts
@@ -88,9 +88,13 @@ export class CommentsListComponent {
 
   set _object(value: any) {
     this.object = value;
+    console.log('this.object',this.object)
     this.guid = this.object.guid;
-    if (this.object.entity_guid)
-      this.guid = this.object.entity_guid;
+    /**
+     * temporary for score. needs to be rearranged
+     */
+    // if (this.object.entity_guid)
+    //   this.guid = this.object.entity_guid;
     this.parent = this.object;
   }
 
@@ -280,7 +284,9 @@ export class CommentsListComponent {
   }
 
   listen() {
+    console.log('socket calling')
     this.socketSubscriptions.comment = this.sockets.subscribe('comment', (data) => {
+      console.log(data)
       const entity_guid = data[0];
       const owner_guid = data[1];
       const guid = data[2];
