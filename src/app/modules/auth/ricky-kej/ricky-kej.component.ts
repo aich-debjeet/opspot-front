8408b94e7066a4e145fd886beac8d274aefe3c2f
Debjeet Aich <debjeet.aich@aeione.com>
2020-04-21 02:04:11 +0530

    update:ricky cage live streaming handles

--- src/app/modules/auth/ricky-kej/ricky-kej.component.ts
+++ src/app/modules/auth/ricky-kej/ricky-kej.component.ts
@@ -8,8 +8,9 @@ import { BrodcastComponent } from './brodcast/brodcast.component';
   styleUrls: ['./ricky-kej.component.scss']
 })
 export class RickyKejComponent implements OnInit, AfterContentInit {
-  tab: string ='home';
+  tab: string = 'home';
   opened: boolean = false;
+  display: string = '';
   constructor(
     private overlayModal: OverlayModalService
   ) { }
@@ -17,9 +18,9 @@ export class RickyKejComponent implements OnInit, AfterContentInit {
   ngOnInit() {
   }
 
-  scrollToElement($element, s:string): void {
-    this.tab= s;
-    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  scrollToElement($element, s: string): void {
+    this.tab = s;
+    $element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
   }
   ngAfterContentInit() {
     (() => {
@@ -33,9 +34,14 @@ export class RickyKejComponent implements OnInit, AfterContentInit {
       });
     })()
   }
-  open(){
-    this.overlayModal.create(BrodcastComponent,{},{
+  open() {
+    this.overlayModal.create(BrodcastComponent, {}, {
       class: 'm-overlay-modal--brodcast-time',
     }).present();
+  }
+  showContent(text: string) {
+    console.log(text);
+    // if(this.display !== '') this.display = '';
+    this.display = text;
   }
 }
