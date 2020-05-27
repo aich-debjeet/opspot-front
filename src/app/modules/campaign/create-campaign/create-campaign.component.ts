93ee0678142c3560171a2bb80a2ae27b8c2b2cdd
Debjeet Aich <debjeet.aich@aeione.com>
2020-05-27 17:36:10 +0530

    Add:create enrollment form structure

--- src/app/modules/campaign/create-campaign/create-campaign.component.ts
+++ src/app/modules/campaign/create-campaign/create-campaign.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormValidator } from '../../../helpers/form.validator';
 
 @Component({
   selector: 'app-create-campaign',
@@ -6,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-campaign.component.scss']
 })
 export class CreateCampaignComponent implements OnInit {
-
-  constructor() {
-    console.log('wants to create a campaign')
+  campaignForm : FormGroup;
+  constructor(private fb: FormBuilder) {
    }
 
   ngOnInit() {
   }
-
+  createForm(){
+  this.campaignForm = this.fb.group({
+    eventTitle: ['', [Validators.required]],
+      eventDesc: ['', [Validators.required]],
+      eventType: ['', [Validators.required]],
+      eventCategory: ['', [Validators.required]],
+      eventLocation: ['', [Validators.required]],
+      eventStartDate: ['', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
+      eventEndDate: ['', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
+      eventStartTime: ['', [Validators.required]],
+      eventEndTime: ['', [Validators.required]],
+      enrollmentStartDate: ['', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
+      enrollmentEndDate: ['', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
+      enrollmentStartTime: ['', [Validators.required]],
+      enrollmentEndTime: ['', [Validators.required]],
+      eventCoverImage: ['', []]
+  });
+  }
 }
