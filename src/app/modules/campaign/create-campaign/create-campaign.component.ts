5a9d1e7636a9d5201ad1b5bd590882606123ada2
Debjeet Aich <debjeet.aich@aeione.com>
2020-06-03 15:04:47 +0530

    ADD:- file uploads for create campaign form

--- src/app/modules/campaign/create-campaign/create-campaign.component.ts
+++ src/app/modules/campaign/create-campaign/create-campaign.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild, ElementRef} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormValidator } from '../../../helpers/form.validator';
 import { OpspotTitle } from '../../../services/ux/title';
+import { AttachmentService } from '../../../services/attachment';
+import { remove as _remove, findIndex as _findIndex } from 'lodash';
 
 @Component({
   selector: 'app-create-campaign',
@@ -10,7 +12,15 @@ import { OpspotTitle } from '../../../services/ux/title';
 })
 export class CreateCampaignComponent implements OnInit {
   campaignForm : FormGroup;
-  constructor(private fb: FormBuilder, private title: OpspotTitle) {
+  imgSrc: '';
+  previewCover: boolean = false;
+  cards = [];
+  bannerGuid: string;
+
+  @ViewChild('file') coverFile: ElementRef;
+  @ViewChild('fileGallery') fileGallery: ElementRef;
+  
+  constructor(private fb: FormBuilder, private title: OpspotTitle, private attachment: AttachmentService,) {
     this.title.setTitle('Campaign-Enrollment');
    }
 
@@ -37,5 +47,61 @@ export class CreateCampaignComponent implements OnInit {
       price: ['', [Validators.required]],
       duration: ['', [Validators.required]]
   });
+  }
+  uploadAttachment(event){
+    console.log('firing',this.coverFile, event, this.coverFile.nativeElement);
+    this.attachment.upload(this.coverFile.nativeElement)
+    .then(guid => {
+      console.log(guid);
+      if(guid){
+        this.bannerGuid = guid;
+        this.previewCover = true;
+        this.imgSrc = this.attachment.getPreview();
+        console.log(this.campaignForm)
+      }
+    })
+
+  }
+
+  removeAttachment() {
+    // console.log(this.coverFile)
+    // console.log(this.campaignForm)
+    this.attachment.remove(this.bannerGuid,this.coverFile.nativeElement).then(guid => {
+      this.previewCover = false;
+      this.coverFile.nativeElement.value = "";
+      this.imgSrc = "";
+      this.campaignForm.get('enrollCoverImage').setValue(null);
+    }).catch(e =>{});
+  }
+
+  uploadGalleryAttachment(event){
+    console.log(event);
+    this.attachment.upload(this.fileGallery.nativeElement)
+    .then(guid => {
+      console.log(guid);
+      console.log(this.fileGallery);
+      if(guid){
+        let guidObj = {};
+        guidObj['guid'] = guid;
+        guidObj['src'] = this.attachment.getPreview();
+        this.cards.push(guidObj);
+        console.log(this.campaignForm)
+      }
+    })
+  }
+
+  removeGalleryAttachment(obj: any){
+    this.attachment.remove(obj.guid, this.fileGallery.nativeElement)
+      .then(guid => {
+        this.cards = _remove(this.cards, function (n) {
+          return n.guid != guid;
+        });
+        if(this.cards.length == 0){
+          this.fileGallery.nativeElement.value = ""; //to empty the input placeholder
+          this.campaignForm.get('gallery').setValue(null);
+        }
+      })
+      .catch(e => {
+      });
   }
 }
