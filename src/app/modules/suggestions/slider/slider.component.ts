eb9e6564648a37ec762ac170a8eb61cfc07e4a68
Gayatri Kawade <gayatrikawade80@gmail.com>
2020-06-03 12:45:18 +0530

    update: suggestion widget

--- src/app/modules/suggestions/slider/slider.component.ts
+++ src/app/modules/suggestions/slider/slider.component.ts
@@ -17,6 +17,46 @@ export class SuggestionsSlider {
     this.load();
   }
 
+  slideConfig = {
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow:1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+
   async load() {
     this.inProgress = true;
     let limit: number = 2;
