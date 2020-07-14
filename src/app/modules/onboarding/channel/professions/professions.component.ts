0fbfe4be6e7e4c4da0430fb056e1018b417bd32a
Gayatri Kawade <gayatrikawade80@gmail.com>
2020-07-14 10:49:26 +0530

    add: professions component

---
+++ src/app/modules/onboarding/channel/professions/professions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professions',
+  templateUrl: './professions.component.html',
+  styleUrls: ['./professions.component.scss']
+})
+export class ProfessionsOnboardingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
