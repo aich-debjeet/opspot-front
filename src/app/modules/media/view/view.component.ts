78552009caa773a04823b7075b5ffec2c29e775e
Debjeet Aich <debjeet.aich@aeione.com>
2020-07-15 15:35:53 +0530

    fix:updation of media display in case of multiple entities inside inner media component

--- src/app/modules/media/view/view.component.ts
+++ src/app/modules/media/view/view.component.ts
@@ -14,6 +14,7 @@ import { PostFormComponent } from '../../forms/post-form/post-form.component';
 import { OverlayModalService } from '../../../services/ux/overlay-modal';
 import { BoostCreatorComponent } from '../../boost/creator/creator.component';
 import { TranslationService } from '../../../services/translation';
+import { find as _find } from 'lodash';
 
 @Component({
   moduleId: module.id,
@@ -316,6 +317,17 @@ export class MediaViewComponent {
   //     this.largeImage = this.entity.custom_data[i].src;
   //   }
   // }
+
+  updateMediaDisplay(currentIndex, data){
+    if(currentIndex != 0){
+      let foundObject = _find(this.entity.custom_data, function(e) {
+        return e.guid === data.guid;
+      });
+      if(foundObject) {
+        this.entity.custom_data.splice(0, 0, this.entity.custom_data.splice(currentIndex, 1)[0]);
+      }
+    }
+  }
 
   async togglePin() {
     this.entity.bookmark = !this.entity.bookmark;
