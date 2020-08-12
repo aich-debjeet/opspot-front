de3ff9ce7481fdbf49d1bdeb6c1b40a1b30c2151
Gayatri Kawade <gayatrikawade80@gmail.com>
2020-08-12 10:22:24 +0530

    add: added secondary contact details

--- src/app/modules/channels/profile-edit/contact/contact.component.ts
+++ src/app/modules/channels/profile-edit/contact/contact.component.ts
@@ -12,13 +12,13 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ContactComponent implements OnInit {
   activeUser = window.Opspot.user;
-  wrongWebsite : boolean = false;
-  inProgress:boolean = false;
+  wrongWebsite: boolean = false;
+  inProgress: boolean = false;
   @Output() updatePercentage: EventEmitter<any> = new EventEmitter();
 
   constructor(
     private client: Client,
-    private router: Router, 
+    private router: Router,
     private toastr: ToastrService
   ) { }
 
@@ -26,15 +26,25 @@ export class ContactComponent implements OnInit {
     location: 'Following',
     email: 'Following',
     phone: 'Following',
-    website: 'Following'
+    website: 'Following',
+    secondary_phone: 'Following'
   };
   toggleLocation;
   toggleEmail;
   togglePhone;
   toggleWebsite;
+  toggleSecondaryphone;
   phoneNumber;
 
-  model: any = {};
+  model: any = {
+    secondary_phone: []
+  };
+
+  showSecMobNumber = false;
+
+  // secondaryMobileNumbers: any[] = [{
+  //   secMobileNumber: '',
+  // }];
 
   ngOnInit() {
     this.load();
@@ -61,31 +71,49 @@ export class ContactComponent implements OnInit {
         this.privacy.website = e;
         this.toggleWebsite = !this.toggleWebsite;
         break;
+
+      case 'secondary_phone':
+        this.privacy.secondary_phone = e;
+        this.toggleSecondaryphone = !this.toggleSecondaryphone;
+        break;
     }
   }
 
   async load() {
     let res = await this.client.get('api/v1/channel/me');
     let response: any = res['channel'];
+    if (response['secondary_phone'] && response['secondary_phone'].length > 0) {
+      this.showSecMobNumber = true;
+    }
+
+    
     this.model.phoneNumber = response['phone'];
     this.model.email = response['email'];
     this.model.location = response['location'];
     this.model.website = response['website'];
+    this.model.secondary_phone = response['secondary_phone'][0];
 
     this.privacy.email = response['email_visibility'] ? `${response['email_visibility'].charAt(0).toUpperCase()}${response['email_visibility'].slice(1)}` : this.privacy.email;
     this.privacy.location = response['location_visibility'] ? `${response['location_visibility'].charAt(0).toUpperCase()}${response['location_visibility'].slice(1)}` : this.privacy.location;
     this.privacy.website = response['website_visibility'] ? `${response['website_visibility'].charAt(0).toUpperCase()}${response['website_visibility'].slice(1)}` : this.privacy.website;
     this.privacy.phone = response['phone_number_visibility'] ? `${response['phone_number_visibility'].charAt(0).toUpperCase()}${response['phone_number_visibility'].slice(1)}` : this.privacy.phone;
+    this.privacy.secondary_phone = response['secondary_phone_visibility'] ? `${response['secondary_phone_visibility'].charAt(0).toUpperCase()}${response['secondary_phone_visibility'].slice(1)}` : this.privacy.secondary_phone;
+
   }
 
   onSubmit() {
-    const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/ ;
-    if(this.model.website.length > 0){
-      if(!regex.test(this.model.website)){
+    const regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+    if (this.model.website.length > 0) {
+      if (!regex.test(this.model.website)) {
         this.wrongWebsite = true;
         return
       } else this.wrongWebsite = false;
     }
+
+    let mobileArray = [];
+    if(this.model.secondary_phone)
+    mobileArray.push(this.model.secondary_phone)
+
     this.inProgress = true;
     const contact = {
       contact_details: {
@@ -93,13 +121,15 @@ export class ContactComponent implements OnInit {
         location: this.model.location ? this.model.location : '',
         phone: this.model.phoneNumber,
         website: this.model.website ? this.model.website : '',
+        secondary_phone: mobileArray,
         location_visibility: this.privacy.location.toLowerCase().split(' ').join(''),
         phone_visibility: this.privacy.phone.toLowerCase().split(' ').join(''),
         email_visibility: this.privacy.email.toLowerCase().split(' ').join(''),
-        website_visibility: this.privacy.website.toLowerCase().split(' ').join('')
+        website_visibility: this.privacy.website.toLowerCase().split(' ').join(''),
+        secondary_phone_visibility: this.privacy.secondary_phone.toLowerCase().split(' ').join('')
       }
     };
-    this.client.post('api/v1/entities/contact_details', contact).then((res:any) => {
+    this.client.post('api/v1/entities/contact_details', contact).then((res: any) => {
       if (res.status === 'success' && res.entities == true) {
         this.client.get('api/v2/onboarding/progress').then((response: any) => {
           this.showSuccess();
@@ -120,9 +150,16 @@ export class ContactComponent implements OnInit {
       timeOut: 3000
     });
   }
-  showFailure(){
+  showFailure() {
     this.toastr.error('Profile could not be updated', '', {
       timeOut: 3000
     });
+  }
+
+  addMobileNumbers() {
+    this.showSecMobNumber = true;
+    // this.secondaryMobileNumbers.push({
+    //   secMobileNumber: '',    
+    // });
   }
 }
