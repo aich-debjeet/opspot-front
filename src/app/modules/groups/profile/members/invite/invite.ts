9fd6a03b430e6778f088e13bc57ecb07400055b2
Gayatri Kawade <gayatrikawade80@gmail.com>
2020-08-18 18:31:52 +0530

    updated: subscribed user list

--- src/app/modules/groups/profile/members/invite/invite.ts
+++ src/app/modules/groups/profile/members/invite/invite.ts
@@ -1,8 +1,11 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 import { Client } from '../../../../../services/api';
 import { GroupsService } from '../../../groups-service';
 import { ToastrService } from 'ngx-toastr';
+import { OpspotHttpClient } from '../../../../../common/api/client.service';
+import { Session } from '../../../../../services/session';
+
 
 
 @Component({
@@ -13,7 +16,7 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: 'invite.html'
 })
 
-export class GroupsProfileMembersInvite {
+export class GroupsProfileMembersInvite implements OnInit{
 
   opspot = window.Opspot;
 
@@ -32,10 +35,18 @@ export class GroupsProfileMembersInvite {
 
   timeout;
 
+  offset: string = '';
+  inProgress: boolean = false;
+  moreData: boolean = true;
+
+  httpSubscription
+
   constructor(
-    public client: Client,
+    public client: OpspotHttpClient,
     public service: GroupsService,
-    private toastr: ToastrService) {
+    private toastr: ToastrService,
+    public session: Session,
+    ) {
     if (window.innerWidth < 775) { this.mobileView = true; }
   }
 
@@ -43,12 +54,15 @@ export class GroupsProfileMembersInvite {
     this.group = value;
   }
 
+  ngOnInit(){
+    this.load(true);
+  }
   invite(user) {
-    if (!user.subscriber) {
-      this.toastr.error('You can only invite users who are subscribed to you');
-      //  alert('You can only invite users who are subscribed to you');
-      return;
-    }
+    // if (!user.subscriber) {
+    //   this.toastr.error('You can only invite users who are subscribed to you');
+    //   //  alert('You can only invite users who are subscribed to you');
+    //   return;
+    // }
 
     this.invited.next(user);
 
@@ -75,32 +89,83 @@ export class GroupsProfileMembersInvite {
     if (this.timeout)
       clearTimeout(this.timeout);
 
-    this.searching = true;
-    if (this.q.charAt(0) !== '@') {
-      this.q = '@' + this.q;
-    }
+    // this.searching = true;
+    // if (this.q.charAt(0) !== '@') {
+    //   this.q = '@' + this.q;
+    // }
 
     var query = this.q;
-    if (query.charAt(0) === '@') {
-      query = query.substr(1);
-    }
+    // if (query.charAt(0) === '@') {
+    //   query = query.substr(1);
+    // }
 
     this.timeout = setTimeout(() => {
-      this.client.get(`api/v2/search/suggest/user`, {
-        q: query,
-        limit: 5,
-        hydrate: 1
-      })
-        .then((success: any) => {
-          if (success.entities) {
-            this.users = success.entities;
+      // this.client.get(`api/v2/search/suggest/user`, {
+      //   q: query,
+      //   limit: 5,
+      //   hydrate: 1
+      // })
+      //   .then((success: any) => {
+      //     if (success.entities) {
+      //       this.users = success.entities;
 
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      //     }
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //   });
+      this.load(true, query)
     }, 600);
   }
 
+  load(refresh: boolean = false, query = null) {
+    if (this.httpSubscription)
+      this.httpSubscription.unsubscribe();
+
+    if (refresh) {
+      this.offset = '';
+      this.moreData = true;
+      this.users = [];
+    }
+
+    var key = "users";
+    let endpoint = `api/v1/subscribe/subscriptions/${this.session.getLoggedInUser().guid}`,
+      params: { limit, offset, q?: string } = { limit: 12, offset: this.offset };
+
+    if (this.q) {
+      endpoint = `api/v2/search/suggest/user`;
+      params.q = query;
+      key = "entities"
+    }
+
+    this.inProgress = true;
+    this.httpSubscription = this.client.get(endpoint, params)
+      .subscribe((response: any) => {
+
+        if (!response[key]) {
+          this.moreData = false;
+          this.inProgress = false;
+          return false;
+        }
+
+        if (refresh) {
+          this.users = response[key];          
+        } else {
+          this.users = this.users.concat(response[key]);
+        }
+
+        if (response['load-next']) {
+          this.offset = response['load-next'];
+        } else {
+          this.moreData = false;
+        }
+
+        this.inProgress = false;
+
+      }, (err) => {
+        this.inProgress = false;
+      });
+  }
+
 }
+
