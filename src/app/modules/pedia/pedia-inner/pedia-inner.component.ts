ae6e30533f255d2e4a12ff357334b9c9fa31d9f2
Debjeet Aich <debjeet.aich@aeione.com>
2020-08-26 19:05:40 +0530

    fix:Cors code

--- src/app/modules/pedia/pedia-inner/pedia-inner.component.ts
+++ src/app/modules/pedia/pedia-inner/pedia-inner.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: 'app-pedia-inner',
@@ -8,12 +7,9 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
   styleUrls: ['./pedia-inner.component.scss']
 })
 export class PediaInnerComponent implements OnInit {
-  props: string ="descriptions|info|sitelinks";
-  uniqId: string ='';
-  wikiApi = `https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&ids=Q127369&props=${this.props}&languages=en&format=json`;
+
   constructor(
     private route: ActivatedRoute,
-    public http: HttpClient
   ) { }
 
   ngOnInit() {
@@ -21,20 +17,9 @@ export class PediaInnerComponent implements OnInit {
     this.route.params.subscribe(params => {
       console.log('params',params['info']);
       if(params['info']){
-        this.uniqId = params['info'];
-        this.getDetails();
+        
       }
     })
-  }
-  getDetails(){
-    this.http.get(this.wikiApi)
-    .subscribe(
-      res => {
-        console.log(res);
-      },
-      err => {
-        console.log(err);
-      });
   }
 
 }
