583d51b3b9cfa48032cd9dccbaeb3d6abdc24d56
Debjeet Aich <debjeet.aich@aeione.com>
2020-08-28 15:16:38 +0530

    Add: Pedia inner page

--- src/app/modules/pedia/pedia.module.ts
+++ src/app/modules/pedia/pedia.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule as NgCommonModule } from '@angular/common';
+import { CommonModule } from '../../common/common.module';
 
 import { PediaRoutingModule } from './pedia-routing.module';
 import { PediaInnerComponent } from './pedia-inner/pedia-inner.component';
@@ -7,6 +8,7 @@ import { PediaInnerComponent } from './pedia-inner/pedia-inner.component';
 @NgModule({
   declarations: [PediaInnerComponent],
   imports: [
+    NgCommonModule,
     CommonModule,
     PediaRoutingModule
   ]
