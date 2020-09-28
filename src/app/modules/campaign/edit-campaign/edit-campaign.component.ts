24cd53b4095feed7585f7c53dd02369b14d4d769
Debjeet Aich <debjeet.aich@aeione.com>
2020-09-28 16:34:30 +0530

    update: enrollment edit form get api integration

--- src/app/modules/campaign/edit-campaign/edit-campaign.component.ts
+++ src/app/modules/campaign/edit-campaign/edit-campaign.component.ts
@@ -6,6 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormValidator } from '../../../helpers/form.validator';
 import { OpspotTitle } from '../../../services/ux/title';
 import { AttachmentService } from '../../../services/attachment';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-edit-campaign',
@@ -15,7 +16,12 @@ import { AttachmentService } from '../../../services/attachment';
 export class EditCampaignComponent implements OnInit {
   campaignDetails: any;
   paramsSubscription: Subscription;
-  campaignForm : FormGroup;
+  campaignForm: FormGroup;
+  displayForm: boolean = false;
+  previewCover: boolean = true;
+  imgSrc: string;
+  cards: Array<any>;
+  orgImgSrc: string;
 
   constructor(
     public client: Client,
@@ -32,34 +38,59 @@ export class EditCampaignComponent implements OnInit {
       }
     });
   }
-  editForm(){
+  editForm(data?) {
+    console.log('form data', data)
     this.campaignForm = this.fb.group({
-        campaignTitle: ['', [Validators.required]],
-        campaignDesc: ['', [Validators.required]],
-        campaignLoc: ['', [Validators.required]],
-        enrollStartDate: ['', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
-        enrollEndDate: ['', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
-        enrollStartTime: ['', [Validators.required]],
-        enrollEndTime: ['', [Validators.required]],
-        enrollCoverImage: ['', []],
-        gender: ['',[Validators.required]],
-        refreshmentMaterials: ['',[Validators.required]],
-        gallery: [''],
-        orgName: ['', [Validators.required]],
-        orgAbout: ['', [Validators.required]],
-        orgPic: ['', [Validators.required]],
-        // allowContact: ['', [Validators.required]],
-        price: ['', [Validators.required]],
-        duration: ['', [Validators.required]]
+      campaignTitle: [data['title'] ? data['title'] : '', [Validators.required]],
+      campaignDesc: [data['description'] ? data['description'] : '', [Validators.required]],
+      campaignLoc: [data['location'] ? data['location'] : '', [Validators.required]],
+      enrollStartDate: [data['start_time_date'] ? this.date(data['start_time_date']) : '', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
+      enrollEndDate: [data['end_time_date'] ? this.date(data['end_time_date']) : '', [Validators.required, FormValidator.validateDate, FormValidator.datevalidation]],
+      enrollStartTime: [data['start_time_date'] ? this.time(data['start_time_date']) : '', [Validators.required]],
+      enrollEndTime: [data['end_time_date'] ? this.time(data['end_time_date']) : '', [Validators.required]],
+      enrollCoverImage: [data['cover_data'] ? this.setCover(data['cover_data'][0].src) : '', [Validators.required]],
+      gender: [data['allowed_gender'] ? data['allowed_gender'] : '', [Validators.required]],
+      refreshmentMaterials: [data['brevarges'] ? data['brevarges'] : '', [Validators.required]],
+      gallery: [data['custom_data'] ? this.setGallery(data['custom_data']) : '', [Validators.required]],
+      orgName: [data['organiser_name'] ? data['organiser_name'] : '', [Validators.required]],
+      orgAbout: [data['organiser_about'] ? data['organiser_about'] : '', [Validators.required]],
+      orgPic: [data['organiser_data'] ? this.setOrgPic(data['organiser_data']) : '', [Validators.required]],
+      // allowContact: ['', [Validators.required]],
+      price: [data['enrollment_fees'] ? data['enrollment_fees'] : '', [Validators.required]],
+      // duration: ['', [Validators.required]]
     });
-    }
+  }
+
+  date(dateString) {
+    let date = new Date(parseInt(dateString));
+    return moment(date).format('DD-MM-YYYY');
+  }
+  time(dateString) {
+    let date = new Date(parseInt(dateString));
+    return moment(date).format('HH:mm');
+  }
+  setCover(image) {
+    this.imgSrc = image;
+    return image;
+  }
+  setGallery(data) {
+    this.cards = data
+    return this.cards;
+  }
+  setOrgPic(data) {
+    this.orgImgSrc = data[0].src;
+    return this.orgImgSrc;
+  }
 
   loadCampaign(guid: string) {
-    this.client.get('api/v1/newsfeed/single/' + guid)
+    this.client.get('api/v3/event/' + guid)
       .then((data: any) => {
         console.log(data);
-        if (data.activity) {
-          this.campaignDetails = data.activity;
+        if (data.event) {
+          console.log('data', data);
+          this.campaignDetails = data.event;
+          this.editForm(this.campaignDetails);
+          this.displayForm = true;
         }
       })
       .catch((e) => {
