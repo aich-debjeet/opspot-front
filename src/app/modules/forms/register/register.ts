9b264b5f04656f6e65b1c10eca46b2643ee6f690
suman-1992 <sumanlatadevi@aeione.com>
2020-10-13 15:56:06 +0530

    registration

--- src/app/modules/forms/register/register.ts
+++ src/app/modules/forms/register/register.ts
@@ -269,11 +269,13 @@ export class RegisterForm {
     let date = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     let month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
     let year = [];
-    let a = new Date().getFullYear() - 13;
+    let a = new Date().getFullYear() - 16;
     let ab = a - 70;
     for (let i: any = a; i >= ab; i--) {
       year.push(i)
     }
+    console.log(a);
+    console.log(year);
     const val = { date, month, year };
     return val;
   }
