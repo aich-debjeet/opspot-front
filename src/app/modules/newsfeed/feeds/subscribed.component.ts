cc2b929daf062de01671d304f0e6ba186e03a78f
Debjeet Aich <debjeet.aich@aeione.com>
2020-10-15 13:54:59 +0530

    Add: Post card for enrollment on home page

--- src/app/modules/newsfeed/feeds/subscribed.component.ts
+++ src/app/modules/newsfeed/feeds/subscribed.component.ts
@@ -22,6 +22,7 @@ import { OverlayModalService } from '../../../services/ux/overlay-modal';
 export class NewsfeedSubscribedComponent {
 
   newsfeed: Array<Object> = [];
+  enroll:any;
   prepended: Array<any> = [];
   offset: string = '';
   showBoostRotator: boolean = true;
@@ -118,9 +119,17 @@ export class NewsfeedSubscribedComponent {
         }
         if (this.newsfeed && !refresh) {
           this.newsfeed = this.newsfeed.concat(data.activity);
+          if(!this.enroll){}
+          this.enroll = this.newsfeed.find(data => data['event_type'] == 'Premium');
+          console.log('this.enroll',this.enroll);
+          
         } else {
 
           this.newsfeed = data.activity;
+          if(!this.enroll){
+            this.enroll = this.newsfeed.find(data => data['event_type'] == 'Premium');
+            console.log('this.enroll',this.enroll);
+          }
         }
         this.offset = data['load-next'];
 
