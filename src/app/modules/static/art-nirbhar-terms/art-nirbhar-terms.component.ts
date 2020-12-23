02085e1c6e51c484d67e0bd3d1904cb7994cbf29
Debjeet Aich <debjeet.aich@aeione.com>
2020-12-23 21:58:48 +0530

    update: scss for terms

--- src/app/modules/static/art-nirbhar-terms/art-nirbhar-terms.component.ts
+++ src/app/modules/static/art-nirbhar-terms/art-nirbhar-terms.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-art-nirbhar-terms',
   templateUrl: './art-nirbhar-terms.component.html',
-  styleUrls: ['./art-nirbhar-terms.component.scss']
+  styleUrls: ['./../static.component.scss']
 })
 export class ArtNirbharTermsComponent implements OnInit {
 
